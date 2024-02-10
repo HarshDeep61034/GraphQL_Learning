a5ac65a988ceab0f91567047b223e30580ebc2dd
Harsh Deep <hdeep61034@gmail.com>
2024-02-10 23:22:12 +0530

    server setup done added docker compose, graphql & prisma

--- social-media-app-backend/src/index.ts
+++ social-media-app-backend/src/index.ts
@@ -1,7 +1,19 @@
 import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
 const app = express();
-//lol
-app.get("/", (_req, res) => res.json({ message: "Server Up and Running!" }));
-app.listen(8000, () => {
-  console.log("Server Listening at PORT 8000");
-});
+import bodyParser from "body-parser";
+
+async function init() {
+  const gqlServer = new ApolloServer({
+    typeDefs: ``,
+    resolvers: {},
+  });
+
+  await gqlServer.start();
+  app.use(bodyParser);
+  app.use("/graphql", expressMiddleware(gqlServer));
+  app.listen(8000, () => {
+    console.log("Server Listening at PORT 8000");
+  });
+}
---
+++ social-media-app-backend/src/prisma/migrations/20240210165810_create_user_table/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+    "profile_image_url" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
