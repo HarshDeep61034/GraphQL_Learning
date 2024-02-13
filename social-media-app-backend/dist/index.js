b3970bb98bed5d76d7bbe3a3f6b57c180db9b919
Harsh Deep <hdeep61034@gmail.com>
2024-02-13 18:35:40 +0530

    graphql & prisma wokring basic api

--- social-media-app-backend/dist/index.js
+++ social-media-app-backend/dist/index.js
@@ -1,12 +1,64 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const server_1 = require("@apollo/server");
+const express4_1 = require("@apollo/server/express4");
 const app = (0, express_1.default)();
-//lol
-app.get("/", (_req, res) => res.json({ message: "Server Up and Running!" }));
-app.listen(8000, () => {
-    console.log("Server Listening at PORT 8000");
-});
+const db_1 = require("./lib/db");
+function init() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const gqlServer = new server_1.ApolloServer({
+            typeDefs: `
+
+    type Query {
+    hello: String
+    say(name: String): String 
+     }
+    
+   type Mutation{
+  createUser(email: String!, firstName: String!, lastName: String!, password: String!): Boolean
+}
+
+`,
+            resolvers: {
+                Query: {
+                    hello: () => `Hello There from Graphql Server`,
+                    say: (_, name) => `Hello ${name}`,
+                },
+                Mutation: {
+                    createUser: (_, { email, firstName, lastName, password, }) => __awaiter(this, void 0, void 0, function* () {
+                        yield db_1.prismaClient.user.create({
+                            data: {
+                                email,
+                                password,
+                                firstName,
+                                lastName,
+                                salt: "H@rsh7017",
+                            },
+                        });
+                        return true;
+                    }),
+                },
+            },
+        });
+        yield gqlServer.start();
+        app.use(express_1.default.json());
+        app.use("/graphql", (0, express4_1.expressMiddleware)(gqlServer));
+        app.listen(8000, () => {
+            console.log("Server Listening at PORT 8000");
+        });
+    });
+}
+init();
--- social-media-app-backend/src/index.ts
+++ social-media-app-backend/src/index.ts
@@ -3,17 +3,62 @@ import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 const app = express();
 import bodyParser from "body-parser";
-
+import { prismaClient } from "./lib/db";
 async function init() {
   const gqlServer = new ApolloServer({
-    typeDefs: ``,
-    resolvers: {},
+    typeDefs: `
+
+    type Query {
+    hello: String
+    say(name: String): String 
+     }
+    
+   type Mutation{
+  createUser(email: String!, firstName: String!, lastName: String!, password: String!): Boolean
+}
+
+`,
+    resolvers: {
+      Query: {
+        hello: () => `Hello There from Graphql Server`,
+        say: (_, name: String) => `Hello ${name}`,
+      },
+      Mutation: {
+        createUser: async (
+          _,
+          {
+            email,
+            firstName,
+            lastName,
+            password,
+          }: {
+            firstName: string;
+            lastName: string;
+            email: string;
+            password: string;
+          },
+        ) => {
+          await prismaClient.user.create({
+            data: {
+              email,
+              password,
+              firstName,
+              lastName,
+              salt: "H@rsh7017",
+            },
+          });
+          return true;
+        },
+      },
+    },
   });
 
   await gqlServer.start();
-  app.use(bodyParser);
+  app.use(express.json());
   app.use("/graphql", expressMiddleware(gqlServer));
   app.listen(8000, () => {
     console.log("Server Listening at PORT 8000");
   });
 }
+
+init();
